94a490148916f5ffd811cb23256dd028c3549a87
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-10-15 13:48:22 -0500

    Basic Scene

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lr;
+    public GameObject[] points;
+
+    public void addPoints(GameObject[] pts){
+        lr.positionCount = pts.Length;
+        this.points = pts;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            lr.SetPosition(i, points[i].transform.position);
+        }
+    }
+}
