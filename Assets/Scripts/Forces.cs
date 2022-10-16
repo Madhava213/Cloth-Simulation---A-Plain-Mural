d4e979df2a9b3ccf3a2cace4c3c9dd0eb4977775
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-10-16 14:47:36 -0500

    Update Forces.cs

--- Assets/Scripts/Forces.cs
+++ Assets/Scripts/Forces.cs
@@ -18,8 +18,8 @@ public class Forces : MonoBehaviour
 
     void VelUpdate(GameObject[] spheres)
     {
-        int sphereLen = spheres.Length();
-        Vector3 velBuffer[] = new Vector3[sphereLen];
+        int sphereLen = spheres.Length;
+        Vector3[] velBuffer = new Vector3[sphereLen];
         for (int i = 0; i < sphereLen / 2; i++)
         {
             for (int j = 0; j < sphereLen / 2; j++) //fill velocity buffer
@@ -35,14 +35,14 @@ public class Forces : MonoBehaviour
             for(int j = 0; j < ; j++)
             {
                 //vector between point and its horizontal neighbor. math to get said horizontal neighbors might be wrong though.
-                Vec3 e = spheres[i*(sphereLen/2)+j+1]-spheres[i*(sphereLen/2)+j]; 
-                double l = Math.sqrt(e.dot(e)); //get length
-                e = e / l; //normalize
-                float v1 = e.dot(spheres[i * (sphereLen / 2) + j].GetComponent<Rigidbody>().velocity);
-                float v2 = e.dot(spheres[i * (sphereLen / 2) + j + 1].GetComponent<Rigidbody>().velocity);
+                Vector3 e = spheres[i*(sphereLen/2)+j+1]-spheres[i*(sphereLen/2)+j]; 
+                float l = e.magnitude;
+                e.Normalize();
+                float v1 = Vector3.Dot(e,spheres[i * (sphereLen / 2) + j].GetComponent<Rigidbody>().velocity);
+                float v2 = Vector3.Dot(e,spheres[i * (sphereLen / 2) + j + 1].GetComponent<Rigidbody>().velocity);
                 float ks = 1; //spring constant, can tweak
                 float kd = 0.1f; //dampening, can tweak
-                Vec3 f = -ks * (spheres[i * (sphereLen / 2) + j].transform.position-l) - kd * (v1 - v2);
+                Vector3 f = -ks * (spheres[i * (sphereLen / 2) + j].transform.position-l) - kd * (v1 - v2);
                 velBuffer[i * (sphereLen / 2) + j] = f; //times e times dt
                 velBuffer[i * (sphereLen / 2) + j + 1] = -f; //times e times dt
             }
@@ -54,14 +54,14 @@ public class Forces : MonoBehaviour
             for (int j = 0; j < ; j++)
             {
                 //vector between point and its horizontal neighbor. math to get said horizontal neighbors might be wrong though.
-                Vec3 e = spheres[i  + j * (sphereLen / 2) + 1] - spheres[i + j * (sphereLen / 2)];
-                double l = Math.sqrt(e.dot(e)); //get length
-                e = e / l; //normalize
-                float v1 = e.dot(spheres[i + j * (sphereLen / 2)].GetComponent<Rigidbody>().velocity);
-                float v2 = e.dot(spheres[i + j * (sphereLen / 2) + 1].GetComponent<Rigidbody>().velocity);
+                Vector3 e = spheres[i  + j * (sphereLen / 2) + 1] - spheres[i + j * (sphereLen / 2)];
+                float l = e.magnitude;
+                e.Normalize();
+                float v1 = Vector3.Dot(e,spheres[i + j * (sphereLen / 2)].GetComponent<Rigidbody>().velocity);
+                float v2 = Vector3.Dot(e,spheres[i + j * (sphereLen / 2) + 1].GetComponent<Rigidbody>().velocity);
                 float ks = 1; //spring constant, can tweak
-                float kd = .1; //dampening, can tweak
-                Vec3 f = -ks * (spheres[i + j * (sphereLen / 2)].transform.position - l) - kd * (v1 - v2);
+                float kd = 0.1f; //dampening, can tweak
+                Vector3 f = -ks * (spheres[i + j * (sphereLen / 2)].transform.position - l) - kd * (v1 - v2);
                 velBuffer[i + j * (sphereLen / 2)] = f; //times e times dt
                 velBuffer[i + j * (sphereLen / 2) + 1] = -f; //times e times dt
             }
