56c3cd02e63d5eb492e6d6c3a3ee004fac990f90
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-10-16 13:19:08 -0500

    Sphere Debug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private LineController line;
+    public GameObject Sphere;
+    private GameObject[] spheres;
+    public int numSpheres = 49;
+    public float sphereDistanceOffset = 1.8f;
+    public Vector3 spawnPos = new Vector3(0.0f,0.0f,0.0f);
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int spheresLength = numSpheres/ 2 + numSpheres / 2 - 1;
+        spheres = new GameObject[spheresLength];
+        for (int i = 0; i < numSpheres/2; i++)
+        {
+            for (int j = 0; j < numSpheres/2; j++)
+            {
+                Vector3 position = new Vector3(spawnPos.x + i/sphereDistanceOffset, spawnPos.y + j/sphereDistanceOffset, spawnPos.z);
+                GameObject newSphere = Instantiate(Sphere, position, Quaternion.identity);
+                newSphere.name = "Sphere " + ((i*numSpheres/2)+j);
+                spheres[i + j] = newSphere;
+            }
+        }
+        // for (int i = 0; i < numSpheres; i++)
+        // {
+        //     if(i+1 < numSpheres){
+        //         line.addPoints(new GameObject[2] { spheres[i], spheres[i+1]});
+        //         LineController newLine = Instantiate(line, spheres[i].transform.position, Quaternion.identity);
+        //         newLine.name = spheres[i].name + " to " + spheres[i+1].name;
+        //     }
+        //     if(i+(numSpheres/2) < numSpheres){
+        //         line.addPoints(new GameObject[2] { spheres[i], spheres[i+(numSpheres/2)]});
+        //         LineController newLine = Instantiate(line, spheres[i].transform.position, Quaternion.identity);
+        //         newLine.name = spheres[i].name + " to " + spheres[i+(numSpheres/2)].name;
+        //     }
+        // }
     }
 
     // Update is called once per frame
