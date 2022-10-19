b7b04c83e17f214225f9e279f158a0bc0f162feb
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-10-18 19:34:25 -0500

    Elasticity Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     private GameObject[,] spheres;
     public int numSpheres = 5;
     private int spheresLength;
+    private float maxDistance;
+    public float maxDistanceMultiplier = 1.0f;
     public float sphereDistanceOffset = 1.8f;
     public Vector3 spawnPos = new Vector3(0.0f,0.0f,0.0f);
     public float ks = 1; //spring constant, can tweak
@@ -20,7 +22,22 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         spheresLength = numSpheres * numSpheres;
+        maxDistance = 1 / sphereDistanceOffset * maxDistanceMultiplier;
         spheres = new GameObject[numSpheres,numSpheres];
+
+        GenerateSpheres();
+
+        GenerateLines();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VelUpdate();
+        ClothElasticity();
+    }
+
+    void GenerateSpheres(){
         for (int i = 0; i < numSpheres; i++)
         {
             for (int j = 0; j < numSpheres; j++)
@@ -31,35 +48,30 @@ public class GameManager : MonoBehaviour
                 spheres[i,j] = newSphere;
             }
         }
+    }
 
+    void GenerateLines(){
         for (int i = 0; i < numSpheres; i++)
-        {
-            for (int j = 0; j < numSpheres; j++)
             {
-                // Vertical Lines
-                if (((i*numSpheres)+ j + 1) % numSpheres != 0)
+                for (int j = 0; j < numSpheres; j++)
                 {
-                    line.addPoints(new GameObject[2] { spheres[i,j], spheres[i,j+1] });
-                    LineController newLine = Instantiate(line, spheres[i,j].transform.position, Quaternion.identity);
-                    newLine.name = spheres[i,j].name + " to " + spheres[i,j+1].name;
-                }
+                    // Vertical Lines
+                    if (((i*numSpheres)+ j + 1) % numSpheres != 0)
+                    {
+                        line.addPoints(new GameObject[2] { spheres[i,j], spheres[i,j+1] });
+                        LineController newLine = Instantiate(line, spheres[i,j].transform.position, Quaternion.identity);
+                        newLine.name = spheres[i,j].name + " to " + spheres[i,j+1].name;
+                    }
 
-                // Horizontal Lines
-                if((((i+1)*numSpheres)+j) < spheresLength){
-                    line.addPoints(new GameObject[2] { spheres[i,j], spheres[i+1,j]});
-                    LineController newLine = Instantiate(line, spheres[i,j].transform.position, Quaternion.identity);
-                    newLine.name = spheres[i,j].name + " to " + spheres[i+1,j].name;
+                    // Horizontal Lines
+                    if((((i+1)*numSpheres)+j) < spheresLength){
+                        line.addPoints(new GameObject[2] { spheres[i,j], spheres[i+1,j]});
+                        LineController newLine = Instantiate(line, spheres[i,j].transform.position, Quaternion.identity);
+                        newLine.name = spheres[i,j].name + " to " + spheres[i+1,j].name;
+                    }
                 }
             }
-        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        VelUpdate();
-    }
-
     void VelUpdate()
     {
         Vector3[,] velBuffer = new Vector3[numSpheres,numSpheres];
@@ -88,7 +100,7 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        // // Horizontal
+        // Horizontal
         for (int i = 0; i < numSpheres - 1; i++)
         {
             for (int j = 0; j < numSpheres; j++)
@@ -113,9 +125,75 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < numSpheres-1; j++) //fill velocity buffer
             {
-                velBuffer[i, j] += gravity;
+                // Add Gravity
+                velBuffer[i, j] += gravity * Time.deltaTime;
+
+                // Update Velocities
                 spheres[i, j].GetComponent<Rigidbody>().velocity = velBuffer[i, j];
             }
         }
     } //velUpdate
+
+    void ClothElasticity(){
+        // Vertical
+        for (int i = 0; i <  numSpheres; i++)
+        {
+            for (int j = 0; j < numSpheres - 1; j++){
+
+                GameObject currSphere = spheres[i,j];
+                GameObject nextSphere = spheres[i,j+1];
+
+                float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
+                float error = Mathf.Abs(dist - maxDistance);
+                Vector3 changeDir = Vector3.zero;
+
+                if (dist > maxDistance)
+                {
+                    changeDir = (currSphere.transform.position - nextSphere.transform.position).normalized;
+                } else if (dist < maxDistance)
+                {
+                    changeDir = (nextSphere.transform.position - currSphere.transform.position).normalized;
+                }
+
+                Vector3 changeAmount = changeDir * error;
+                if (((i*numSpheres)+ j + 2) % numSpheres != 0){
+                    currSphere.transform.position -= changeAmount * 0.5f;
+                    spheres[i,j] = currSphere;
+                    nextSphere.transform.position += changeAmount * 0.5f;
+                    spheres[i,j + 1] = nextSphere;
+                }
+                else{
+                    currSphere.transform.position -= changeAmount * 0.5f;
+                    spheres[i,j] = currSphere;
+                }
+            }
+        }
+
+        // Horizontal
+        for (int i = 0; i <  numSpheres-1; i++)
+        {
+            for (int j = 0; j < numSpheres - 1; j++){
+
+                GameObject currSphere = spheres[i,j];
+                GameObject nextSphere = spheres[i+1,j];
+
+                float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
+                float error = Mathf.Abs(dist - maxDistance);
+                Vector3 changeDir = Vector3.zero;
+
+                if (dist > maxDistance)
+                {
+                    changeDir = (currSphere.transform.position - nextSphere.transform.position).normalized;
+                } else if (dist < maxDistance)
+                {
+                    changeDir = (nextSphere.transform.position - currSphere.transform.position).normalized;
+                }
+                Vector3 changeAmount = changeDir * error;
+                currSphere.transform.position -= changeAmount * 0.5f;
+                spheres[i,j] = currSphere;
+                nextSphere.transform.position += changeAmount * 0.5f;
+                spheres[i+1,j] = nextSphere;
+            }
+        }
+    }
 }
