672bd03475db887b094a67a545cba8e36087aefe
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-10-19 21:45:27 -0500

    Physics Rectification

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,9 @@ public class GameManager : MonoBehaviour
     private float maxDistance;
     public float maxDistanceMultiplier = 1.0f;
     public float sphereDistanceOffset = 1.8f;
+    public float restLength = 1/1.8f;
     public Vector3 spawnPos = new Vector3(0.0f,0.0f,0.0f);
-    public float ks = 1; //spring constant, can tweak
+    public float ks = 1.0f; //spring constant, can tweak
     public float kd = 0.1f; //dampening, can tweak
     public Vector3 gravity = new Vector3(0,-0.1f,0); //Gravity
 
@@ -34,7 +35,6 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         VelUpdate();
-        ClothElasticity();
     }
 
     void GenerateSpheres(){
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < numSpheres; j++)
             {
-                Vector3 position = new Vector3(spawnPos.x + i/sphereDistanceOffset, spawnPos.y, spawnPos.z + j/sphereDistanceOffset);
+                Vector3 position = new Vector3(spawnPos.x + i/sphereDistanceOffset, spawnPos.y , spawnPos.z + j/sphereDistanceOffset);
                 GameObject newSphere = Instantiate(Sphere, position, Quaternion.identity);
                 newSphere.name = "Sphere " + ((i*numSpheres)+j);
                 spheres[i,j] = newSphere;
@@ -88,15 +88,14 @@ public class GameManager : MonoBehaviour
         {
             for(int j = 0; j < numSpheres - 1; j++)
             {
-                //vector between point and its horizontal neighbor. math to get said horizontal neighbors might be wrong though.
-                    Vector3 e = spheres[i,j+1].transform.position-spheres[i,j].transform.position; 
-                    float l = e.magnitude;
-                    e.Normalize();
-                    float v1 = Vector3.Dot(e,spheres[i,j].GetComponent<Rigidbody>().velocity);
-                    float v2 = Vector3.Dot(e,spheres[i,j + 1].GetComponent<Rigidbody>().velocity);
-                    float f = -ks * ((1/sphereDistanceOffset)-l) - kd * (v1 - v2);
-                    velBuffer[i,j] += e * f * Time.deltaTime; //times e times dt
-                    velBuffer[i,j + 1] -= -e * f * Time.deltaTime; //times e times dt
+                Vector3 e = spheres[i,j+1].transform.position-spheres[i,j].transform.position; 
+                float l = e.magnitude;
+                e.Normalize();
+                float v1 = Vector3.Dot(e,spheres[i,j].GetComponent<Rigidbody>().velocity);
+                float v2 = Vector3.Dot(e,spheres[i,j + 1].GetComponent<Rigidbody>().velocity);
+                float f = -ks * ((restLength) - l) - kd * (v1 - v2);
+                velBuffer[i,j] += e * f * Time.deltaTime; //times e times dt
+                velBuffer[i,j + 1] -= e * f * Time.deltaTime; //times e times dt
             }
         }
 
@@ -105,16 +104,15 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < numSpheres; j++)
             {
-                //vector between point and its horizontal neighbor. math to get said horizontal neighbors might be wrong though.
                 {
                     Vector3 e = spheres[i+1,j].transform.position - spheres[i,j].transform.position;
                     float l = e.magnitude;
                     e.Normalize();
                     float v1 = Vector3.Dot(e,spheres[i,j].GetComponent<Rigidbody>().velocity);
                     float v2 = Vector3.Dot(e,spheres[i+1,j].GetComponent<Rigidbody>().velocity);
-                    float f = -ks * ((1/sphereDistanceOffset) - l) - kd * (v1 - v2);
+                    float f = -ks * ((restLength) - l) - kd * (v1 - v2);
                     velBuffer[i,j] += e * f * Time.deltaTime; //times e times dt
-                    velBuffer[i + 1,j] -= -e * f * Time.deltaTime; //times e times dt
+                    velBuffer[i + 1,j] -= e * f * Time.deltaTime; //times e times dt
                 }
             }
         }
@@ -125,17 +123,8 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < numSpheres-1; j++) //fill velocity buffer
             {
-
-                GameObject currSphere = spheres[i,j];
-                GameObject nextSphere = spheres[i,j+1];
-
-                float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
-                
-                if (dist < maxDistance)
-                {
-                    // Add Gravity
-                    velBuffer[i, j] += gravity;
-                }
+                // Add Gravity
+                velBuffer[i, j] += gravity * Time.deltaTime;
                 // Update Velocities
                 spheres[i, j].GetComponent<Rigidbody>().velocity = velBuffer[i, j];
 
@@ -143,68 +132,68 @@ public class GameManager : MonoBehaviour
         }
     } //velUpdate
 
-    void ClothElasticity(){
-        // Vertical
-        for (int i = 0; i <  numSpheres; i++)
-        {
-            for (int j = 0; j < numSpheres - 1; j++){
+    // void ClothElasticity(){
+    //     // Vertical
+    //     for (int i = 0; i <  numSpheres; i++)
+    //     {
+    //         for (int j = 0; j < numSpheres - 1; j++){
 
-                GameObject currSphere = spheres[i,j];
-                GameObject nextSphere = spheres[i,j+1];
+    //             GameObject currSphere = spheres[i,j];
+    //             GameObject nextSphere = spheres[i,j+1];
 
-                float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
-                float error = Mathf.Abs(dist - maxDistance);
-                Vector3 changeDir = Vector3.zero;
+    //             float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
+    //             float error = Mathf.Abs(dist - maxDistance);
+    //             Vector3 changeDir = Vector3.zero;
 
-                if (dist > maxDistance)
-                {
-                    changeDir = (currSphere.transform.position - nextSphere.transform.position).normalized;
-                } else if (dist < maxDistance)
-                {
-                    changeDir = (nextSphere.transform.position - currSphere.transform.position).normalized;
-                    error = 0;
-                }
+    //             if (dist > maxDistance)
+    //             {
+    //                 changeDir = (currSphere.transform.position - nextSphere.transform.position).normalized;
+    //             } else if (dist < maxDistance)
+    //             {
+    //                 changeDir = (nextSphere.transform.position - currSphere.transform.position).normalized;
+    //                 error = 0;
+    //             }
 
-                Vector3 changeAmount = changeDir * error;
-                if (((i*numSpheres)+ j + 2) % numSpheres != 0){
-                    currSphere.transform.position -= changeAmount;
-                    spheres[i,j] = currSphere;
-                    nextSphere.transform.position += changeAmount;
-                    spheres[i,j + 1] = nextSphere;
-                }
-                else{
-                    currSphere.transform.position -= changeAmount;
-                    spheres[i,j] = currSphere;
-                }
-            }
-        }
+    //             Vector3 changeAmount = changeDir * error;
+    //             if (((i*numSpheres)+ j + 2) % numSpheres != 0){
+    //                 currSphere.transform.position -= changeAmount;
+    //                 spheres[i,j] = currSphere;
+    //                 nextSphere.transform.position += changeAmount;
+    //                 spheres[i,j + 1] = nextSphere;
+    //             }
+    //             else{
+    //                 currSphere.transform.position -= changeAmount;
+    //                 spheres[i,j] = currSphere;
+    //             }
+    //         }
+    //     }
 
-        // Horizontal
-        for (int i = 0; i <  numSpheres-1; i++)
-        {
-            for (int j = 0; j < numSpheres - 1; j++){
+    //     // Horizontal
+    //     for (int i = 0; i <  numSpheres-1; i++)
+    //     {
+    //         for (int j = 0; j < numSpheres - 1; j++){
 
-                GameObject currSphere = spheres[i,j];
-                GameObject nextSphere = spheres[i+1,j];
+    //             GameObject currSphere = spheres[i,j];
+    //             GameObject nextSphere = spheres[i+1,j];
 
-                float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
-                float error = Mathf.Abs(dist - maxDistance);
-                Vector3 changeDir = Vector3.zero;
+    //             float dist = (currSphere.transform.position - nextSphere.transform.position).magnitude;
+    //             float error = Mathf.Abs(dist - maxDistance);
+    //             Vector3 changeDir = Vector3.zero;
 
-                if (dist > maxDistance)
-                {
-                    changeDir = (currSphere.transform.position - nextSphere.transform.position).normalized;
-                } else if (dist < maxDistance)
-                {
-                    changeDir = (nextSphere.transform.position - currSphere.transform.position).normalized;
-                    error = 0;
-                }
-                Vector3 changeAmount = changeDir * error;
-                currSphere.transform.position -= changeAmount;
-                spheres[i,j] = currSphere;
-                nextSphere.transform.position += changeAmount;
-                spheres[i+1,j] = nextSphere;
-            }
-        }
-    }
+    //             if (dist > maxDistance)
+    //             {
+    //                 changeDir = (currSphere.transform.position - nextSphere.transform.position).normalized;
+    //             } else if (dist < maxDistance)
+    //             {
+    //                 changeDir = (nextSphere.transform.position - currSphere.transform.position).normalized;
+    //                 error = 0;
+    //             }
+    //             Vector3 changeAmount = changeDir * error;
+    //             currSphere.transform.position -= changeAmount;
+    //             spheres[i,j] = currSphere;
+    //             nextSphere.transform.position += changeAmount;
+    //             spheres[i+1,j] = nextSphere;
+    //         }
+    //     }
+    // }
 }
