638d01307384144162a7667e47bbf3618900cfcc
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-10-20 21:56:10 -0500

    Fan Added, Final Draft, Website Created, Player Movement Upgrade

--- Assets/Scripts/CamShooter.cs
+++ Assets/Scripts/CamShooter.cs
@@ -1,43 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CamShooter : MonoBehaviour
 {
+    public Transform playerBody;
+    public CharacterController controller;
+    public float speed = 12f;
     public GameObject bullet;
     public float shootPower = 1f;
     public float moveOffset = 0.01f;
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
+    public TextMeshProUGUI fanStatus;
+    private float fanSpeed = 0.0f;
+    private RaycastHit fanHit;
+    private Collider m_Collider;
+
+    void Start()
+    {
+        // Lock the cursor to prevent it from going out of window
+        Cursor.lockState = CursorLockMode.Locked;
+        m_Collider = GetComponent<Collider>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        // Get Mouse Input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Clamp Rotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Apply Mouse Look Movement
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        // Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Physics.BoxCast(m_Collider.bounds.center, transform.localScale, transform.forward, out fanHit, transform.rotation, 10))
+        {
+            fanHit.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * fanSpeed);
+        }
+
         if (Input.GetMouseButtonDown(0)) {
-            GameObject shell = Instantiate(bullet, transform.position, transform.rotation);
+            GameObject shell = Instantiate(bullet, transform.position + transform.forward, transform.rotation);
             Vector3 shootDir = transform.forward;
             shootDir.y += 0.1f;
             shell.GetComponent<Rigidbody>().AddForce(shootDir * shootPower);
         }
 
-        // Change Position
-        Vector3 newPos = transform.position;
-        if(Input.GetKey("a")){
-            newPos.x -= moveOffset;
+        if(Input.GetKey("1")){
+                fanStatus.text = "Fan : Off";
+                fanSpeed = 0.0f;
+            }
+        if(Input.GetKey("2")){
+            fanStatus.text = "Fan : Low";
+            fanSpeed = 1.0f;
         }
-        if(Input.GetKey("d")){
-            newPos.x += moveOffset;
-        }
-        if(Input.GetKey("w")){
-            newPos.z += moveOffset;
-        }
-        if(Input.GetKey("s")){
-            newPos.z -= moveOffset;
-        }
+        if(Input.GetKey("3")){
+                fanStatus.text = "Fan : Med";
+                fanSpeed = 2.0f;
+            }
+        if(Input.GetKey("4")){
+                fanStatus.text = "Fan : High";
+                fanSpeed = 3.0f;
+            }
+        
         if(Input.GetKey("q")){
-            newPos.y += moveOffset;
+            Application.Quit();
         }
-        if(Input.GetKey("e")){
-            newPos.y -= moveOffset;
-        }
-        transform.position = newPos;
     }
+
+
 }
---
+++ README.md
@@ -0,0 +1,40 @@
+# Cloth Simulation - A Plain Mural
+
+## Authors
+
+-   Madhava Raveendra
+-   Skylar Volden
+
+## Description
+
+## Features
+
+## Code
+
+We are using Unity3D Game Engine for the implementation of this project.
+
+## Execution
+
+Use the button below to navigate to the source code.
+Find the 'Build' folder and run the executable inside.
+
+Controls :
+Mouse : Look Around
+ASWD/Arrow Keys : Move Around
+1 : Fan OFF
+2 : Fan LOW
+3 : Fan MID
+4 : Fan HIGH
+q : Quit Application
+
+## Difficulties
+
+## Video
+
+## Images
+
+<img width="624" alt="Cloth Simulation" src="">
+
+## References/Links
+
+-   Cloth Holder 3D Asset: https://sketchfab.com/3d-models/banner-holder-bfcc5bfd393742d595bd13770c6c16e5
